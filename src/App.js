c97ad552721a3fbf6c23f4f01f3acc7655afc741
bsandeepk <bsandeepk509@gmail.com>
2024-04-18 03:04:05 +0000

    commit message

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {Heading, CustomButton} from './styledComponents'
+
+const App = () => (
+  <>
+    <Heading>Hello World</Heading>
+    <CustomButton type="button">Click</CustomButton>
+    <CustomButton type="button" outline>
+      Out Line Button
+    </CustomButton>
+  </>
+)
+
+export default App
